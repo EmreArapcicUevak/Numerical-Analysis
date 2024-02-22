c63d8c13be44204e5a9e4b2faae72002a9d5a693
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2024-02-22 15:54:50 +0100

    Change the first homework so that the time it took for each section gets plotted out as well

--- PolynomialOptimization/FirstHomework.jl
+++ PolynomialOptimization/FirstHomework.jl
@@ -1,3 +1,4 @@
+using Plots
 function f(n :: Integer, coefficients :: Union{AbstractArray, Tuple}, x :: Real)
     @assert length(coefficients) ≥ n
     local value = 0
@@ -23,17 +24,35 @@ function fOptimized(n :: Integer, coefficients :: Union{AbstractArray, Tuple}, x
 end
 
 randomCoefficients = rand(1_000_000_000)
-for n ∈ map(x -> 10^x, 2:9)
+
+allDegrees = map(x -> 10^x, 1:9) # You can change this for a smaller range if you want to test the code faster or add more points for better scatters.
+time = Dict(
+    "Optimized" => [],
+    "Unoptimized" => []
+)
+
+for (index, n) ∈ enumerate(allDegrees)
     println("Function comparisson for a $n degree polynomial")
     
-    for x₀ ∈ LinRange(0,1,3)
+    local totalTime = [0.0, 0.0]
+
+    for x₀ ∈ 0:0.5:1
         local result, time, garbage... = @timed fOptimized(n, randomCoefficients, x₀) 
+        totalTime[1] = time > totalTime[1] ? time : totalTime[1]
+
         println("f₁($(x₀)) = $(result): Time taken $(time) seconds")
 
         local result, time, garbage... = @timed f(n, randomCoefficients, x₀) 
+        totalTime[2] = time > totalTime[2] ? time : totalTime[2]
+
         println("f₂($(x₀)) = $(result): Time taken $(time) seconds")
         println("-"^20)
     end
 
-    println("*"^50)
+    append!(time["Optimized"], totalTime[1])
+    append!(time["Unoptimized"], totalTime[2])
+    println("*"^20)
 end 
+
+scatter(allDegrees, time["Optimized"], label="Optimized", xlabel="Degree of the polynomial", ylabel="Time taken (s)", title="Optimized vs Unoptimized function")
+scatter!(allDegrees, time["Unoptimized"], label="Unoptimized")
