d8886fb977fbab7d5ff0ffa537cbf1c839027d57
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2024-03-01 23:08:38 +0100

    Add problems.pdf and all solution files needed to start work on this weeks homework

---
+++ Homeworks/Homework2/Solutions/Problem1.jl
@@ -0,0 +1,3 @@
+push!(LOAD_PATH, pwd()*"/Modules/")
+using BisectionMethodModule, Revise, Plots, ShowPointModule, HornerMethodModule, NewtonMethodModule
+plotlyjs() # Set PlotlyJS as the backend for Plots.jl
