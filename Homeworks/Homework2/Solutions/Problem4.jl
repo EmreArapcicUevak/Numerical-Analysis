4c76f9d0f41db7f31f12e67eb3100f05d8f69cd8
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2024-03-02 12:18:34 +0100

    Start work on problem 4

--- Homeworks/Homework2/Solutions/Problem4.jl
+++ Homeworks/Homework2/Solutions/Problem4.jl
@@ -1,3 +1,9 @@
 push!(LOAD_PATH, pwd()*"/Modules/")
 using BisectionMethodModule, Revise, Plots, ShowPointModule, HornerMethodModule, NewtonMethodModule
 plotlyjs() # Set PlotlyJS as the backend for Plots.jl
+
+f(x) = sin(x) + x^2 * cos(x) - x^2 - x
+f_prime(x) = (1 + 2x) * (cos(x) - 1) -x^2 * sin(x)
+
+result = NewtonMethod(f, f_prime, 1)
+ShowPoint(f,(result.c,0); domain = (-20, 20))
