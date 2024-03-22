0afe2a799ca2bb91aec53d52f0819da18def57cf
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2024-03-22 17:58:49 +0100

    Use unix operators

--- Modules/ElementaryMatrixOperationsModule.jl
+++ Modules/ElementaryMatrixOperationsModule.jl
@@ -1,25 +1,25 @@
-module ElemenetaryMatrixOperationsModule
+module ElementaryMatrixOperationsModule
     using MatrixAnalysisModule
     export SwapRows!, AddRowMultiple!, MultiplyRow!
 
     validType = Union{Rational, AbstractFloat}
 
     function SwapRows!(A :: Matrix{T}, i :: Int, j :: Int) where T <: validType
-        @assert i > 0 && i <= size(A,1) "i must be a valid row index"
-        @assert j > 0 && j <= size(A,1) "j must be a valid row index"
+        @assert i > 0 && i ≤ size(A,1) "i must be a valid row index"
+        @assert j > 0 && j ≤ size(A,1) "j must be a valid row index"
 
         A[i,:], A[j,:] = A[j,:], A[i,:]
     end
 
     function AddRowMultiple!(A :: Matrix{T}, i :: Int, j :: Int, α :: T) where T <: validType
-        @assert i > 0 && i <= size(A,1) "i must be a valid row index"
-        @assert j > 0 && j <= size(A,1) "j must be a valid row index"
+        @assert i > 0 && i ≤ size(A,1) "i must be a valid row index"
+        @assert j > 0 && j ≤ size(A,1) "j must be a valid row index"
 
         A[i,:] += α * A[j,:]
     end
 
     function MultiplyRow!(A :: Matrix{T}, i :: Int, α :: T) where T <: validType
-        @assert i > 0 && i <= size(A,1) "i must be a valid row index"
+        @assert i > 0 && i ≤ size(A,1) "i must be a valid row index"
 
         A[i,:] *= α
     end
