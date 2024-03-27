5a49964d4f4a830a9649689b85b66f5bd704e107
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2024-03-27 19:25:44 +0100

    Add some more examples

--- Sections/Systems of Equations/LinearSystem.jl
+++ Sections/Systems of Equations/LinearSystem.jl
@@ -18,28 +18,32 @@ result = PLUFactorization(A)
 A
 result.U
 result.L
-
+result.P
 
 A = Rational[3 -7 -2 2; -3 5 1 0; 6 -4 0 -5; -9 5 -5 12]
 result = PLUFactorization(A)
 A
 result.U
 result.L
+result.P
 
-A = Float64[2 -4 -2 3; 6 -9 -5 8; 2 -7 -3 9; 4 -2 -2 -1; -6 3 3 4]
+
+A = Rational[2 -4 -2 3; 6 -9 -5 8; 2 -7 -3 9; 4 -2 -2 -1; -6 3 3 4]
 result = PLUFactorization(A)
 A
 result.U
 result.L
 result.L * result.U
 
-A = Float64[2 3 1; 4 1 4; 3 4 6]
+A = Rational[2 3 1; 4 1 4; 3 4 6]
 result = PLUFactorization(A)
 A
 result.U
 result.L
+result.P
 
-b = Float64[-4, 9, 0]
+b = Rational[-4, 9, 0]
+SolvePLU(result.P, result.L, result.U, b)
 y = SolveLowerDiagonal(result.L, b)
 x = SolveUpperDiagonal(result.U, y)
 
@@ -54,9 +58,11 @@ result.L * result.U
 
 A = Rational[3 -7 -2 2; -3 5 1 0; 6 -4 0 -5; -9 5 -5 12]
 result = PLUFactorization(A)
+A
 result.U
 result.L
 result.P
+
 b = Rational[-9, 5, 7, 11]
 x = SolvePLU(result.P, result.L, result.U, b)
 result.L * result.U * x 
@@ -78,5 +84,19 @@ x = SolvePLU(result, b)
 
 A = Rational[10 -7 0; -3 2 6; 5 -1 5]
 b = Rational[1,2,3]
+result = PLUFactorization(A)
+x = SolvePLU(result, b)
+
+A = Rational[2 1 -1; -3 -1 2; -2 1 2]
+b = Rational[1,2,3]
+result = PLUFactorization(A)
+result.U
+result.L
+result.P
+x = SolvePLU(result, b)
+x
+
+A = Float64[21 -10 -1 0; -10 21 -10 -1; -1 -10 21 -10; 0 -1 -10 21]
+b = Float64[50, 0, 0, 0]
 result = PLUFactorization(A)
 x = SolvePLU(result, b)
