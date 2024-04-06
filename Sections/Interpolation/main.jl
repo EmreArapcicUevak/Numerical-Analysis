472438670ffd4d124dffb3fc874620fc3be3a004
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2024-04-06 15:49:41 +0200

    Add some more examples for interpolation

--- Sections/Interpolation/main.jl
+++ Sections/Interpolation/main.jl
@@ -8,6 +8,7 @@ plotlyjs() # Set PlotlyJS as the backend for Plots.jl
 
 ###################################################
 using LinearSystemModule
+using InterpolationModule
 #### Your code goes here ####
 
 A = [1 0 0 0; 1 π/2 (π/2)^2 (π/2)^3; 1 π (π)^2 (π)^3; 1 3π/2 (3π/2)^2 (3π/2)^3]
@@ -23,3 +24,50 @@ poly = coef[1] .+ coef[2] .* x .+ coef[3] .* x.^2 .+ coef[4] .* x.^3
 plot(x, y, label = "sin(x)")
 scatter!([0, pi/2, pi, 3pi/2],[0,1,0,-1], label = "Value Points")
 plot!(x, poly, label = "Interpolated Polynomial")
+
+x = Float64[2, 2.75, 4]
+y = 1 ./ x
+polyCoef = RecursiveInterpolation(x, y)
+poly(X) = polyCoef[1] + polyCoef[2] * (X - x[1]) + polyCoef[3] * (X - x[1]) * (X - x[2])
+poly(3)
+poly(2)
+A = [1 2 4; 1 2.75 2.75^2; 1 4 4^2]
+result = PLUFactorization(A)
+coef = SolvePLU(result, y)
+coef
+
+polyGuess = LagrangeInterpolation(x, y)
+polyGuess(3)
+
+coef = LinearInterpolation(x, y)
+
+x = Float64[0,1,2,3]
+y = Float64[3,5,7,9]
+coef = NewtonDivider(x, y)
+
+poly(X) = coef[1] + coef[2] * (X - x[1]) + coef[3] * (X - x[1]) * (X - x[2]) + coef[4] * (X - x[1]) * (X - x[2]) * (X - x[3])
+xExp = LinRange(0, 3, 9000)
+yExp = poly.(xExp)
+
+scatter(x, y, label = "Value Points")
+plot!(xExp, yExp, label = "Interpolated Polynomial")
+
+
+x = Float64[0,pi/2, pi, 3pi/2, 2pi]
+y = sin.(x)
+coef = NewtonDivider(x, y)
+
+poly(X) = coef[1] + coef[2] * (X - x[1]) + coef[3] * (X - x[1]) * (X - x[2]) + coef[4] * (X - x[1]) * (X - x[2]) * (X - x[3])
+xExp = LinRange(0, 2pi, 9000)
+yExp = poly.(xExp)
+scatter(x, y, label = "Value Points")
+plot!(xExp, yExp, label = "Interpolated Polynomial")
+
+
+x = Float64[0,1,2,3]
+y = Float64[3,5,7,9]
+coef = NewtonDivider(x, y)
+
+poly(X) = coef[1] + coef[2] * (X - x[1]) + coef[3] * (X - x[1]) * (X - x[2]) + coef[4] * (X - x[1]) * (X - x[2]) * (X - x[3])
+xExp = LinRange(0, 3, 9000)
+yExp = poly.(xExp)
